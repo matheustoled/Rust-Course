908e660994a4e71d21d1b4556929b4050c814141
Matheus-Note-Linux <matheustole304@gmail.com>
2026-01-13 12:42:44 -0300

    Function exercise completed

--- topics/function/exercises/src/lib.rs
+++ topics/function/exercises/src/lib.rs
@@ -1,7 +1,9 @@
-pub fn mul() {
-    todo!();
+pub fn mul(x: u32, y: u32) -> u32 {
+    //return x * y;
+    x * y
 }
 
-pub fn div() {
-    todo!();
+pub fn div(x: u32, y: u32) -> u32 {
+    //return x / y;
+    x / y
 }
