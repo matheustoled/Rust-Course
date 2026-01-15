e3cad748df6b8a8a57486a8f76881920a7656a27
Matheus-Note-Linux <matheustole304@gmail.com>
2026-01-15 12:24:52 -0300

    Array and Slice exercise completed

--- topics/array/exercises/src/lib.rs
+++ topics/array/exercises/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn zeros() -> [u32; 100] {
-    todo!();
+    return [0; 100];
 }
 
 pub fn first_3(s: &[u32]) -> &[u32] {
-    todo!();
+    &s[..3]
 }
 
 pub fn last_3(s: &[u32]) -> &[u32] {
-    todo!();
+    let l = s.len();
+    &s[l - 3..]
 }
