dfc3cfb1e068b2c10243824d5013b401625467a0
Matheus-Note-Linux <matheustole304@gmail.com>
2026-01-21 18:23:28 -0300

    Vector exercise completed

--- topics/vector/exercises/src/lib.rs
+++ topics/vector/exercises/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn init(x: u32, y: u32, z: u32) -> Vec<u32> {
-    todo!();
+    vec![x, y, z];
+    // let mut v: Vec<u32> = Vec::new();
+    // v.push(x);
+    // v.push(y);
+    // v.push(z);
 }
