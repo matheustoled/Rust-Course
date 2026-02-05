6dcad3cc56448c9077ebf1bb9d02bfb9f3835108
Matheus-Note-Linux <matheustole304@gmail.com>
2026-02-05 14:57:52 -0300

    Loop exercise completed

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,19 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum = 0;
+    for i in 0..nums.len() {
+        sum += nums[i];
+    }
+    sum
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let v = vec![i; n];
+    
+    // let mut v = vec![];
+
+    // for _ in 0..n {
+    //     v.push(i);
+    // }
+
+    v
 }
