464bb57673f53fc414372fce04f8998b4b0ee0ba
Matheus-Note-Linux <matheustole304@gmail.com>
2026-02-10 13:29:56 -0300

    Question Operator exercise completed

--- topics/question/exercises/src/lib.rs
+++ topics/question/exercises/src/lib.rs
@@ -6,5 +6,11 @@ fn parse(s: &str) -> Result<u32, String> {
 }
 
 pub fn sum(nums: &[&str]) -> Result<u32, String> {
-    todo!();
+    let mut total = 0;
+
+    for n in nums {
+        total += parse(n)?;
+    }
+
+    Ok(total)
 }
