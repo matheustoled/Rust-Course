478a3da617d8d5a983993e163e54c71e91795ae4
Matheus-Note-Linux <matheustole304@gmail.com>
2026-02-10 12:50:44 -0300

    Unwrap and Expect exercise completed

--- topics/unwrap/exercises/src/lib.rs
+++ topics/unwrap/exercises/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn parse_and_add(a: &str, b: &str) -> u32 {
-    todo!();
+    let x: u32 = a.parse().expect("Failed to parse variable");
+    let y: u32 = b.parse().expect("Failed to parse variable");
+    x + y
 }
 
 pub fn unwrap_and_add(x: Option<u32>, y: Option<u32>) -> u32 {
-    todo!();
+    x.unwrap() + y.unwrap()
 }
